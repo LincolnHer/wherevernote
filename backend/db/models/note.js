61750973e09943641f8262d7bae3600ee1bdb073
LincolnHer <lincolnher15@gmail.com>
2022-04-05 11:08:16 -0500

    created models for all tables

---
+++ backend/db/models/note.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Note = sequelize.define('Note', {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    notebookId:  {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  }, {});
+  Note.associate = function(models) {
+    // associations can be defined here
+    Note.belongsTo(models.User, { foreignKey: 'userId' })
+    Note.belongsTo(models.Notebook, { foreignKey: 'notebookId' })
+    Note.belongsTo(models.Tag, { foreignKey: 'noteId' })
+  };
+  return Note;
+};
