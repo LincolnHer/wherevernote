3900cdfc00226a2e251e2a6949aeb8b5ace75707
LincolnHer <lincolnher15@gmail.com>
2022-04-06 18:10:27 -0500

    fixed associations and recommented csrf function

--- backend/db/models/tag.js
+++ backend/db/models/tag.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Tag.associate = function(models) {
     // associations can be defined here
-    // Tag.hasMany(models.Note, { foreignKey: 'noteId' })
+    Tag.belongsTo(models.Note, { foreignKey: 'noteId' })
     Tag.belongsTo(models.User, { foreignKey: 'userId' })
   };
   return Tag;
