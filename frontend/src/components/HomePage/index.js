b1df6098ccd41e29569fe22ed2d0a31bafb61498
LincolnHer <lincolnher15@gmail.com>
2022-04-08 07:38:05 -0500

    minor modification to home page and sidebar page

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from "react-router-dom";
+import Sidebar from "../SidebarPage";
 import './HomePage.css'
 
 function HomePage() {
   const sessionUser = useSelector(state => state.session.user);
-  
+
   if (!sessionUser) {
    return <Redirect to="/" />
   }
 
   return (
+    <>
       <h1>Hello From Home Page</h1>
+      <Sidebar />
+    </>
   )
 }
 
