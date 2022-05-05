453076d98a75f0a6f945edea1ddda2152112cdcb
LincolnHer <lincolnher15@gmail.com>
2022-05-05 18:38:25 -0500

    added actions and thunks for notes feature

---
+++ frontend/src/store/notes.js
@@ -0,0 +1,116 @@
+import { csrfFetch } from './csrf';
+
+//Action variables
+const GET_NOTES = 'notes/GET_NOTES'
+const GET_NOTE = 'notes/GET_NOTE'
+const POST_NOTE = 'notes/POST_NOTE'
+const PUT_NOTE = 'notes/PUT_NOTE'
+const DELETE_NOTE = 'notes/DELETE_NOTE'
+
+//Action Creators
+const getUserNotes = (note) => {
+  return {
+    type: GET_NOTES,
+    payload: note
+  }
+}
+
+const getNote = (noteId) => {
+  return {
+    type: GET_NOTE,
+    payload: noteId
+  }
+}
+
+const postNote = (note) => {
+  return {
+    type: POST_NOTE,
+    payload: note
+  }
+}
+
+const putNote = (note) => {
+  return {
+    type: PUT_NOTE,
+    payload: note
+  }
+}
+
+const removeNote = (note) => {
+  return {
+    type: DELETE_NOTE,
+    payload: note
+  }
+}
+
+//Thunks
+
+//GET all notes owned by user
+export const getNotes = (userId) => async dispatch => {
+  const res = await fetch(`/api/notes/${userId}`);
+
+  if (res.ok) {
+    const allNotes = await res.json();
+    dispatch(getUserNotes(allNotes));
+  }
+};
+
+//GET a single note
+export const getSingleNote = (noteId) => async dispatch => {
+  const res = await fetch(`/api/notes/note/${noteId}`)
+
+  if (res.ok) {
+    const singleNote = await res.json();
+    dispatch(getNote(singleNote));
+  }
+}
+
+//POST create a note
+export const createNote = (note) => async dispatch => {
+  const res = await csrfFetch('/api/notes', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(note)
+  })
+
+  if (res.ok) {
+    const newNote = await res.json();
+    dispatch(postNote(newNote));
+  }
+};
+
+//PUT edit a note
+export const editNote = (note, noteId) => async dispatch => {
+  const res = await csrfFetch(`/api/notebooks/notebook/${noteId}`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(note)
+  })
+
+  if (res.ok) {
+    const updatedNote = await res.json();
+    dispatch(putNote(updatedNote))
+  }
+}
+
+//DELETE delete a note
+export const deleteNote = (note) => async dispatch => {
+  const res = await csrfFetch(`/api/notes/note/${note}`, {
+    method: 'DELETE'
+  });
+
+  if (res.ok) {
+    const oldNote = await res.json();
+    dispatch(removeNote(note));
+  }
+}
+
+const initialState = {};
+
+export default function notesReducer (state = initialState, action) {
+  let newState;
+  switch (action.type) {
+    default:
+      return state
+  }
+}
